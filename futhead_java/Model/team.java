6437cf6c1654577c8c7b4833446cfe49a16bd646
ict2017 <nt.trungict@gmail.com>
2017-05-20 18:44:02 +0700

    add team model

---
+++ futhead_java/Model/team.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class team {
+	private String teamid;
+	private String name;
+	private String league;
+	
+	public String getTeamid() {
+		return teamid;
+	}
+	public void setTeamid(String teamid) {
+		this.teamid = teamid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLeague() {
+		return league;
+	}
+	public void setLeague(String league) {
+		this.league = league;
+	}
+}
