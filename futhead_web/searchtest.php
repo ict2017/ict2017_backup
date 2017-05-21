815d01984410be8dafbe0fa8cec16fe6f87bc73f
ict2017 <nt.trungict@gmail.com>
2017-05-21 15:46:26 +0700

    add search function

---
+++ futhead_web/searchtest.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Demo Search </title>
+    </head>
+    <body>
+        <div align="right">
+            <form action="search.php" method="get">
+                Search: <input type="text" name="search" />
+                <input type="submit" name="ok" value="search" />
+            </form>
+        </div>
+</html>
+
+<!--
+<html>
+<body>
+
+<form action="search.php?go" method="POST"  id="searchform">
+<input name="name" type="text" placeholder="Type here">
+<input name="submit" type="submit" value="Search">
+</form>
+</body>
+</html>
